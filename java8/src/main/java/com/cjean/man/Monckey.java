afce843035b4243a53f81035ac563d759f067193
cjeanGith <1417261875@qq.com>
2020-10-11 18:28:06 +0800

    独立源码

---
+++ java8/src/main/java/com/cjean/man/Monckey.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: Monckey
+ * Author:   14172
+ * Date:     2020/10/11 18:09
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.cjean.man;
+
+/**
+ * @author 14172
+ * @create 2020/10/11
+ * @since 1.0.0
+ */
+public class Monckey {
+    public static void main(String[] args) {
+        System.out.println("2222");
+    }
+}
